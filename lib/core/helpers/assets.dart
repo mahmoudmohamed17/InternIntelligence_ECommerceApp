3d43c7f88a8f4eb6024cab917bf465e3e35bff42
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-18 18:59:54 +0200

    add assets

---
+++ lib/core/helpers/assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBeauty
+  /// assets/images/beauty.jpg
+  static const String imagesBeauty = "assets/images/beauty.jpg";
+
+  /// Assets for imagesElectronics
+  /// assets/images/electronics.jpg
+  static const String imagesElectronics = "assets/images/electronics.jpg";
+
+  /// Assets for imagesMen
+  /// assets/images/men.jpg
+  static const String imagesMen = "assets/images/men.jpg";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+
+  /// Assets for imagesWomen
+  /// assets/images/women.jpg
+  static const String imagesWomen = "assets/images/women.jpg";
+}
+
