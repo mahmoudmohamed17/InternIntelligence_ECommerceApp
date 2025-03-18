62c21d8cf8df7b4b8905085bc4f883426ef156ab
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-18 17:46:17 +0200

    add app colors and text styles

---
+++ lib/core/helpers/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff1D86F5);
+  static const secondaryColor = Color(0xffB3C9F2);
+  static const deepPrimaryColor = Color(0xff4C5155);
+  static const itemBackgroundColor = Color(0xffF1F1F1);
+  static const primaryTextColor = Color(0xff0E0E0E);
+  static const secondaryTextColor = Color(0xff707070);
+}
---
+++ lib/core/helpers/app_text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle bold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle semibold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600, // SemiBold
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle medium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500, // Medium
+  );
+
+  static const TextStyle semibold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600, // SemiBold
+  );
+
+  static const TextStyle regular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle bold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle medium12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500, // Medium
+  );
+}
