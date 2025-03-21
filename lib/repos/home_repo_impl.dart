fca377cc87fa4e7ece8360eef91c8e13704cc36e
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-21 21:33:33 +0200

    make home repo impl

--- lib/repos/home_repo_impl.dart
+++ lib/repos/home_repo_impl.dart
@@ -1,13 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:e_commerce_app/core/datasources/home_remote_data_source.dart';
+import 'package:e_commerce_app/core/errors/api_server_failure.dart';
 import 'package:e_commerce_app/core/errors/failure.dart';
 import 'package:e_commerce_app/core/helpers/product_entity.dart';
 import 'package:e_commerce_app/repos/home_repo.dart';
 
 class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpl({required this.homeRemoteDataSource});
   @override
   Future<Either<Failure, List<ProductEntity>>> getProducts({
     required String endpoint,
-  }) {
-    throw UnimplementedError();
+  }) async {
+    try {
+      var products = await homeRemoteDataSource.getProducts(endpoint: endpoint);
+      return right(products);
+    } on DioException catch (e) {
+      return left(ApiServerFailure.fromDioException(e));
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
   }
 }
