ae22cebe8f64ef0275101a9d88b618801dff267e
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-23 21:25:43 +0200

    small refactor

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-
-    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    const MyApp(),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
