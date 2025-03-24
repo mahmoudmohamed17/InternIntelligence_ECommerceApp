ba1a34a0afb379e4a9701491b4a73589d109c5a8
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-24 17:06:49 +0200

    finish product cubit

--- lib/managers/product_cubit/product_cubit.dart
+++ lib/managers/product_cubit/product_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce_app/core/helpers/product_entity.dart';
+import 'package:e_commerce_app/services/firebase_service.dart';
 part 'product_state.dart';
 
 class ProductCubit extends Cubit<ProductState> {
@@ -7,4 +8,34 @@ class ProductCubit extends Cubit<ProductState> {
   List<ProductEntity> cartProducts = [];
 
   ProductCubit() : super(ProductInitial());
+
+  final _firebaseService = FirebaseService();
+
+  Future<void> addToFavorites(ProductEntity product) async {
+    product.isAddedToFavorites = true;
+    await _firebaseService.updateProduct(product);
+    favoritesProducts.add(product);
+    emit(ProductFavoritesSuccess());
+  }
+
+  Future<void> removeFromFavorites(ProductEntity product) async {
+    product.isAddedToFavorites = false;
+    await _firebaseService.updateProduct(product);
+    favoritesProducts.remove(product);
+    emit(ProductFavoritesSuccess());
+  }
+
+  Future<void> addToCart(ProductEntity product) async {
+    product.isAddedToCart = true;
+    await _firebaseService.updateProduct(product);
+    cartProducts.remove(product);
+    emit(ProductCartSuccess());
+  }
+
+  Future<void> removeFromCart(ProductEntity product) async {
+    product.isAddedToCart = false;
+    await _firebaseService.updateProduct(product);
+    cartProducts.remove(product);
+    emit(ProductCartSuccess());
+  }
 }
--- lib/managers/product_cubit/product_state.dart
+++ lib/managers/product_cubit/product_state.dart
@@ -3,4 +3,5 @@ part of 'product_cubit.dart';
 class ProductState {}
 
 final class ProductInitial extends ProductState {}
-final class ProductSuccess extends ProductState {}
+final class ProductFavoritesSuccess extends ProductState {}
+final class ProductCartSuccess extends ProductState {}
