5aaf8bcc4eaea925d00114c3e9992aef39dc4583
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-24 17:11:44 +0200

    provide product cubit to main view and product details view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/core/utils/app_routing.dart';
 import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/managers/home_cubit/home_cubit.dart';
+import 'package:e_commerce_app/managers/product_cubit/product_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,8 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomeCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => HomeCubit()),
+        BlocProvider(create: (context) => ProductCubit()),
+      ],
       child: MaterialApp.router(
         theme: ThemeData(fontFamily: 'Poppins'),
         debugShowCheckedModeBanner: false,
--- lib/views/product_details_view.dart
+++ lib/views/product_details_view.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce_app/core/helpers/product_entity.dart';
+import 'package:e_commerce_app/managers/product_cubit/product_cubit.dart';
 import 'package:e_commerce_app/widgets/product_details_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class ProductDetailsView extends StatelessWidget {
@@ -9,9 +11,13 @@ class ProductDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var product = GoRouterState.of(context).extra as ProductEntity;
-    return  Scaffold(
-      backgroundColor: Colors.white,
-      body: ProductDetailsViewBody(product: product,),
+    return BlocBuilder<ProductCubit, ProductState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: ProductDetailsViewBody(product: product),
+        );
+      },
     );
   }
 }
