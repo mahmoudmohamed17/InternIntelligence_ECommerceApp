7d3e10308ccd499a6584424019ad2ebd955aa0e7
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-25 13:35:29 +0200

    small edits to notifications service

--- lib/views/notifications_view.dart
+++ lib/views/notifications_view.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_app/core/helpers/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class NotificationsView extends StatelessWidget {
   const NotificationsView({super.key});
@@ -8,8 +10,11 @@ class NotificationsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notifications',),
-        titleTextStyle: AppTextStyles.bold24,
+        title: const Text('Notifications', style: AppTextStyles.bold24,),
+        centerTitle: true,
+        leading: IconButton(onPressed: () {
+          context.replace('/');
+        }, icon: const Icon(FontAwesomeIcons.chevronLeft)),
       ),
     );
   }
