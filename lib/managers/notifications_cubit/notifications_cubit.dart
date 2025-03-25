dbe50b9174ccc2b6d70c969f2c5c3b6beb22285f
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-25 21:38:47 +0200

    complete notifications cubit

--- lib/managers/notifications_cubit/notifications_cubit.dart
+++ lib/managers/notifications_cubit/notifications_cubit.dart
@@ -1,6 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:e_commerce_app/models/notification_item_model.dart';
 part 'notifications_state.dart';
 
 class NotificationsCubit extends Cubit<NotificationsState> {
   NotificationsCubit() : super(NotificationsInitial());
+  List<NotificationItemModel> notifications = [];
+
+  void addNotfication(NotificationItemModel notification) {
+    notifications.add(notification);
+    emitNotificationsState();
+  }
+
+  void removeNotfication(NotificationItemModel notification) {
+    notifications.remove(notification);
+    emitNotificationsState();
+  }
+
+  emitNotificationsState() {
+    if (notifications.isEmpty) {
+      emit(NotificationsInitial());
+    } else {
+      emit(
+        NotificationsFilled(notifications: List.unmodifiable(notifications)),
+      );
+    }
+  }
 }
--- lib/managers/notifications_cubit/notifications_state.dart
+++ lib/managers/notifications_cubit/notifications_state.dart
@@ -3,4 +3,8 @@ part of 'notifications_cubit.dart';
 class NotificationsState {}
 
 final class NotificationsInitial extends NotificationsState {}
-final class NotificationsFilled extends NotificationsState {}
+
+final class NotificationsFilled extends NotificationsState {
+  final List<NotificationItemModel> notifications;
+  NotificationsFilled({required this.notifications});
+}
